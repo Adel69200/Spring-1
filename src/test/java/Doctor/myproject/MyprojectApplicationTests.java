303a3235560eb7adcb76b64afc58d7e7ddab483a
Adel <adel6994700@gmail.com>
2021-06-17 11:27:31 +0200

    challenge ok

---
+++ src/test/java/Doctor/myproject/MyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package Doctor.myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
